fa3cb6efb0bd6c19db86c21f8a9daa4acda4716f
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-09-05 09:58:34 -0400

    First push of Eclipse files

---
+++ ch01_Test/src/ch01_Test/TestApp.java
@@ -0,0 +1,19 @@
+package ch01_Test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		System.out.println("Success!");
+		System.out.println("Success!");
+		System.out.println("Success!");
+		
+		double q = 10;
+		double price = 49.50;
+		double t = (price * q);
+		
+		System.out.printf("%.2f", t);
+		
+		
+	}
+
+}
