d575c1228c81499545e3bac87427dcc6f39d726a
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-09-10 08:38:11 -0400

    added comments to FactorialCalculatorApp

--- ch04_prj4-2_FactorialCalculator/src/FactorialCalculatorApp.java
+++ ch04_prj4-2_FactorialCalculator/src/FactorialCalculatorApp.java
@@ -12,13 +12,14 @@ public class FactorialCalculatorApp {
 		while (choice.equalsIgnoreCase("y")) {
 			long factorial = 1;
 
-			//modified to accept up to 20 (max for program)
+			//modified to accept up to 20 (max for program) and calculates factorial of given number
 			System.out.print("Enter an Integer that's greater than 0 and less than 21: ");
 			int number = input.nextInt();
 			for (int i = 1; i < number ; i++) {
 				factorial = factorial * (i + 1);
 			}
 			
+			//prints results of factorial calculation
 			System.out.println("The Factorial of " + number + " is " + factorial + ".");
 			System.out.println();
 			
