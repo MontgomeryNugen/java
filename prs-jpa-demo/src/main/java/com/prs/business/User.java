7e48a008bb9fb76669ee83edaa019dc7720c019d
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-09-26 11:10:31 -0400

    small changes

--- prs-jpa-demo/src/main/java/com/prs/business/User.java
+++ prs-jpa-demo/src/main/java/com/prs/business/User.java
@@ -1,7 +1,14 @@
 package com.prs.business;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String userName;
 	private String password;
--- prs-jpa-demo/src/main/java/com/prs/business/UserDB.java
+++ prs-jpa-demo/src/main/java/com/prs/business/UserDB.java
@@ -1,15 +1,31 @@
 package com.prs.business;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import com.prs.db.DBUtil;
 
 
 public class UserDB {
 	
 	public UserDB() {
+		
 	}
 		
-	public ArrayList<User> list() {
-		ArrayList<User> users = new ArrayList<>();
+	public List<User> list() {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		
+		List<User> users = new ArrayList<>();
+		try {
+			Query q = em.createQuery("SELECT u FROM User u");
+			users = q.getResultList();
+		}
+		finally {
+			em.close();
+		}
 		
 		return users;
 	}
